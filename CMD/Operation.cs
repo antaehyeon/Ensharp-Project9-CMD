c982eb7942ac5badfadb7836b6fbb747ff350fd6
antaehyeon <antaehyeon@live.co.kr>
2016-06-24 15:46:23 +0900

    <16.06.24 15:45> 1. CMD 기능구현완료
    <해야될것들>
    1. 예외처리

--- CMD/Operation.cs
+++ CMD/Operation.cs
@@ -20,6 +20,9 @@ namespace CMD
         string instruction = "";
         string currentPath = "";
 
+        const int COPY = 1;
+        const int MOVE = 2;
+
         // 명령어가 한번이라도 입력되면 true
         bool instructionCheck = false;
         // cls 명령어가 입력되었을 경우
@@ -44,7 +47,6 @@ namespace CMD
                 clsCheck = false;
                 if (cdCheck) return;
                 Console.WriteLine();
-
             }
             if (cdCheck)
             {
@@ -84,6 +86,13 @@ namespace CMD
                 case "cd\\":
                     currentPath = rootDirectory(currentPath);
                     cdCheck = true;
+                    break;
+                case "copy":
+                    //copyAndMove(instruction, COPY);
+                    copyMove2(instruction, COPY);
+                    break;
+                case "move":
+                    copyMove2(instruction, MOVE);
                     break;
                 default:
                     driveCheck(SpaceDivision[0]);
@@ -206,7 +215,6 @@ namespace CMD
         {
             bool onlyComma = false;
             bool directoryExist = false;
-            bool containSemi = false;
 
             string setPath = "";
 
@@ -262,8 +270,6 @@ namespace CMD
             {
                 notExistPath(instruction);
             }
-
-
         }
 
         public bool existPath(string path)
@@ -324,6 +330,381 @@ namespace CMD
             }
         }
 
+        // 현재경로와 다른곳에서 copy를 진행할 경우
+        // 경로를 하나만 쓸경우, 쓴곳의 파일을 현재 폴더로 붙여넣는다
+        // 경로에서 이미 해당파일의 경로를 하나만 쓸경우 예외처리
+        // 경로에서 하나만 썼는데, 없는 파일일 경우 예외처리
+
+        public void copyAndMove(string instruction, int mode)
+        {
+            int count = 0;
+
+            string firstFile = "";
+            string secondFile = "";
+
+            string choice = "";
+
+            string[] combineSpace = instruction.Split(' ');
+
+            foreach(string str in combineSpace)
+            {
+                if (str.Length > 0) count++;
+            }
+
+            if(count.Equals(3))
+            {
+                firstFile = existFile(combineSpace[1]);
+                secondFile = existFile(combineSpace[2]);
+
+                if (string.IsNullOrWhiteSpace(firstFile))
+                {
+                    Console.WriteLine("지정된 파일을 찾을 수 없습니다.");
+                    return;
+                }
+                // 공백이면 파일이 존재하지 않는다
+                if (string.IsNullOrWhiteSpace(secondFile))
+                {
+                    string targetPath = "";
+                    FileInfo originFile = new FileInfo(firstFile);
+                    // 해당경로에 파일이 존재하지 않는다면
+                    if (!existPath(combineSpace[2]))
+                    {
+                        targetPath = existFile(combineSpace[2]);
+                        //targetPath = combineSpace[2];
+                        //originFile.CopyTo(targetPath, true);
+                        //Console.WriteLine("\t\t1개 파일이 복사되었습니다.\n");
+                        copyAndPasteFunction(originFile, targetPath, mode, 1);
+                    }
+                    else
+                    {
+                        targetPath = currentPath + "\\" + combineSpace[2];
+                    }
+
+                    if (targetPath.Equals(""))
+                    {
+                        //originFile.CopyTo(targetPath, true);
+                        //Console.WriteLine("\t\t1개 파일이 복사되었습니다.\n");
+                        copyAndPasteFunction(originFile, targetPath, mode, 1);
+                    }
+                }
+                else
+                {
+                    FileInfo originFile = new FileInfo(firstFile);
+
+                    Console.Write("{0}을(를) 덮어쓰시겠습니까? (Yes/No/All): ", combineSpace[2]);
+                    choice = Console.ReadLine();
+
+                    if (mode.Equals(MOVE))
+                    {
+                        FileInfo destination = new FileInfo(secondFile);
+                        destination.Delete();
+                    }
+
+                    switch (choice.ToLower())
+                    {
+                        case "yes":
+                        case "ye":
+                        case "y":
+                            //originFile.CopyTo(secondFile, true);
+                            //Console.WriteLine("\t\t1개 파일이 복사되었습니다.\n");
+                            copyAndPasteFunction(originFile, secondFile, mode, 1);
+                            break;
+                        case "no":
+                        case "n":
+                            if (mode.Equals(COPY)) Console.WriteLine("\t\t0개 파일이 복사되었습니다.\n");
+                            else Console.WriteLine("\t\t0개 파일을 이동했습니다.\n");
+                            break;
+                        case "all":
+                            //originFile.CopyTo(secondFile, true);
+                            //Console.WriteLine("\t\t1개 파일이 복사되었습니다.\n");
+                            copyAndPasteFunction(originFile, secondFile, mode, 1);
+                            break;
+                    }
+                }
+            }
+            else if (count.Equals(2))
+            {
+                string destinationPath = "";
+
+                firstFile = existFile(combineSpace[1]);
+                destinationPath = currentDirectory.FullName;
+
+                FileInfo file = new FileInfo(firstFile);
+                destinationPath = destinationPath + "\\" + file.Name;
+
+                secondFile = existFile(destinationPath);
+
+                // 만약 존재하지 않는 파일을 복사하려고 한다면
+                if (firstFile.Equals(""))
+                {
+                    Console.WriteLine("지정된 파일을 찾을 수 없습니다.");
+                    return;
+                }
+                else if (secondFile.Equals(""))
+                {
+                    //file.CopyTo(destinationPath, true);
+                    //Console.WriteLine("\t\t1개 파일이 복사되었습니다.");
+                    copyAndPasteFunction(file, secondFile, mode, 1);
+                }
+                else
+                {
+                    Console.Write("{0}을(를) 덮어쓰시겠습니까? (Yes/No/All): ", secondFile);
+                    choice = Console.ReadLine();
+
+                    if (mode.Equals(MOVE))
+                    {
+                        FileInfo destination = new FileInfo(secondFile);
+                        destination.Delete();
+                    }
+
+                    switch (choice.ToLower())
+                    {
+                        case "yes":
+                        case "ye":
+                        case "y":
+                            //file.CopyTo(secondFile, true);
+                            //Console.WriteLine("\t\t1개 파일이 복사되었습니다.\n");
+                            copyAndPasteFunction(file, secondFile, mode, 1);
+                            break;
+                        case "no":
+                        case "n":
+                            if (mode.Equals(COPY)) Console.WriteLine("\t\t0개 파일이 복사되었습니다.\n");
+                            else Console.WriteLine("\t\t0개 파일을 이동했습니다.\n");
+                            break;
+                        case "all":
+                            //file.CopyTo(secondFile, true);
+                            //Console.WriteLine("\t\t1개 파일이 복사되었습니다.\n");
+                            copyAndPasteFunction(file, secondFile, mode, 1);
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("명령 구문이 올바르지 않습니다.\n");
+                return;
+            }
+        }
+
+        public void copyMove2(string instruction, int mode)
+        {
+            int count = 0;
+
+            string startFileInfo = "";
+            string destinationFileInfo = "";
+            // 해당 명령문이 경로인지, 파일명인지 체크하기 위함
+            bool firstInstruction = false;
+            bool secondInstruction = false;
+
+            const int NODUPLICATION = -1;
+
+            string choice = "";
+
+            string[] combineSpace = instruction.Split(' ');
+
+            foreach (string str in combineSpace)
+            {
+                if (str.Length > 0) count++;
+            }
+
+            if(count.Equals(3))
+            {
+                // 해당 명령문이 파일명인지 경로인지 체크한다음에
+                firstInstruction = pathCheck(combineSpace[1]);
+                secondInstruction = pathCheck(combineSpace[2]);
+
+                // 둘다 파일명일 경우
+                if (!firstInstruction && !secondInstruction)
+                {
+                    startFileInfo = currentPath + "\\" + combineSpace[1];
+                    destinationFileInfo = currentPath + "\\" + combineSpace[2];
+                }
+                // 앞에만 경로일 경우
+                else if (firstInstruction && !secondInstruction)
+                {
+                    startFileInfo = combineSpace[1];
+                    destinationFileInfo = currentPath + "\\" + combineSpace[2];
+                }
+                // 앞에가 파일명 뒤에가 경로일 경우
+                else if (!firstInstruction && secondInstruction)
+                {
+                    startFileInfo = currentPath + "\\" + combineSpace[1];
+                    destinationFileInfo = combineSpace[2];
+                }
+                // 둘다 경로일 경우
+                else if (firstInstruction && secondInstruction)
+                {
+                    startFileInfo = combineSpace[1];
+                    destinationFileInfo = combineSpace[2];
+                }
+
+                FileInfo startFile = new FileInfo(startFileInfo);
+                FileInfo destinationFile = new FileInfo(destinationFileInfo);
+
+                // DirectoryInfo
+                DirectoryInfo startDirectory = new DirectoryInfo(startFile.Directory.ToString());
+                DirectoryInfo destinationDirectory = new DirectoryInfo(destinationFile.Directory.ToString());
+
+                // 각각 폴더에 있는 파일들을 전부 끌어옴
+                FileInfo[] startDirectoryItems = startDirectory.GetFiles();
+                FileInfo[] destinationDirectoryItems = destinationDirectory.GetFiles();
+
+                // 해당 폴더에 파일이 없다면
+                if(!existFile2(startDirectoryItems, startFile.Name))
+                {
+                    Console.WriteLine("지정된 파일을 찾을 수 없습니다.");
+                    return;
+                }
+                // 해당 폴더에 같은이름의 파일이 이미 있다면
+                if(existFile2(destinationDirectoryItems, destinationFile.Name))
+                {
+                    Console.Write("{0}을(를) 덮어쓰시겠습니까? (Yes/No/All): ", destinationFile.FullName);
+                    string answer = Console.ReadLine();
+                    answer = answer.ToLower();
+                    switch (answer)
+                    {
+                        case "yes":
+                        case "ye":
+                        case "y":
+                        case "all":
+                            destinationFile.Delete();
+                            copyAndPasteFunction(startFile, destinationFileInfo, mode, 1);
+                            break;
+                        case "no":
+                        case "n":
+                            copyAndPasteFunction(null, null, mode, NODUPLICATION);
+                            break;
+                    }
+                    return;
+                }
+                copyAndPasteFunction(startFile, destinationFileInfo, mode, 1);
+            }
+            else if(count.Equals(2))
+            {
+                startFileInfo = combineSpace[1];
+
+                FileInfo startFile = new FileInfo(startFileInfo);
+
+                destinationFileInfo = currentPath + "\\" + startFile.Name;
+
+                FileInfo destinationFile = new FileInfo(destinationFileInfo);
+
+                DirectoryInfo destinationDirectory = new DirectoryInfo(destinationFile.Directory.ToString());
+
+                FileInfo[] destinationDirectoryItems = destinationDirectory.GetFiles();
+
+                if (existFile2(destinationDirectoryItems, destinationFile.Name))
+                {
+                    Console.Write("{0}을(를) 덮어쓰시겠습니까? (Yes/No/All): ", destinationFile.FullName);
+                    string answer = Console.ReadLine();
+                    answer = answer.ToLower();
+                    switch (answer)
+                    {
+                        case "yes":
+                        case "ye":
+                        case "y":
+                        case "all":
+                            destinationFile.Delete();
+                            copyAndPasteFunction(startFile, destinationFileInfo, mode, 1);
+                            break;
+                        case "no":
+                        case "n":
+                            copyAndPasteFunction(null, null, mode, NODUPLICATION);
+                            break;
+                    }
+                    return;
+                }
+                copyAndPasteFunction(startFile, destinationFileInfo, mode, 1);
+            }
+            else
+            {
+                Console.WriteLine("명령 구문이 올바르지 않습니다.");
+            }
+        }
+
+
+        public string checkPath(string str)
+        {
+            string path = "";
+
+            if (str.Contains("c:\\"))
+            {
+                DirectoryInfo directory = new DirectoryInfo(str);
+                path = directory.Parent.FullName.ToString();
+                return path;
+            }
+            else
+            {
+                //currentDirectoryFileList = currentDirectory.GetFiles();
+                return currentDirectory.FullName.ToString();
+            }
+        }
+
+        public bool pathCheck(string str)
+        {
+            str = str.ToLower();
+            // 해당 명령문이 경로일경우
+            if (str.Contains("c:\\")) { return true; }
+            else { return false; }
+        }
+
+        public string existFile(string str)
+        {
+            FileInfo[] currentDirectoryFileList;
+
+            DirectoryInfo directory = new DirectoryInfo(checkPath(str));
+            currentDirectoryFileList = directory.GetFiles();
+
+            foreach (var fileName in currentDirectoryFileList)
+            {
+                if (fileName.Name.ToString().Equals(str))
+                {
+                    return fileName.FullName;
+                }
+                if (fileName.FullName.ToString().Equals(str))
+                {
+                    return fileName.FullName;
+                }
+            }
+            return "";
+        }
+
+        // 해당 폴더에 파일이 존재하는지 체크
+        public bool existFile2(FileInfo[] items, string fileName)
+        {
+            foreach(var str in items)
+            {
+                if (fileName.Equals(str.Name))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void copyAndPasteFunction(FileInfo file, string path, int mode, int num)
+        {
+            if (mode.Equals(COPY))
+            {
+                if (num.Equals(-1))
+                {
+                    Console.WriteLine("\t\t0개 파일이 복사되었습니다/");
+                    return;
+                }
+                file.CopyTo(path, true);
+                Console.WriteLine("\t\t{0}개 파일이 복사되었습니다.", num);
+            }
+            else if(mode.Equals(MOVE))
+            {
+                if (num.Equals(-1))
+                {
+                    Console.WriteLine("\t\t0개 파일이 이동했습니다.");
+                    return;
+                }
+                file.MoveTo(path);
+                Console.WriteLine("\t\t{0}개 파일이 이동했습니다.", num);
+            }
+        }
 
 
     }
